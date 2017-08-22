6200486d77648b33c3e4d7d269dff4e940af5d60
pengfei <baipengfei1234@126.com>
2017-08-22 16:07:26 +0800

    Rainbow commit twice.

---
+++ threadLearn/src/threadSea/TestInner.java
@@ -0,0 +1,12 @@
+package threadSea;
+
+
+/**
+ * Created by pengfei on 2017/8/22.
+ */
+public class TestInner {
+
+    public static void main(String[] args) {
+        StaticClass.InnerClass obj=new StaticClass.InnerClass();// static inner class.
+    }
+}
